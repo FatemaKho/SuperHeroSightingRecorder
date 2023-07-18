7c44a0da1f926d5df59a2e091824d8b0fcf9325f
Lupita Peralta <cstuart@email.com>
2023-07-18 17:57:54 -0500

    changes

--- SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/dao/OrganizationDaoDB.java
+++ SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/dao/OrganizationDaoDB.java
@@ -26,13 +26,24 @@ public class OrganizationDaoDB implements OrganizationDao {
     @Autowired
     JdbcTemplate jdbc;
 
+    final String GET_ORGANIZATION_BY_ID = "SELECT * FROM organization WHERE organizationPK = ?";
+    final String GET_ALL_ORGANIZATIONS = "SELECT * FROM organization";
+    final String GET_HEROS_BY_ORGANIZATION = "SELECT DISTINCT h.* FROM hero h INNER JOIN heroorganization ho " +
+            " ON h.heroPK = ho.heroPK WHERE organizationPK = ?";
+    final String INSERT_ORGANIZATION = "INSERT INTO organization (organizationName, type, description, " +
+            " organizationAddress, phone, contactInfo) VALUES(?, ?, ?, ?, ?, ?)";
+    final String GET_INSERT_ID = "SELECT LAST_INSERT_ID()";
+    final String UPDATE_ORGANIZATION = "UPDATE organization SET organizationName = ?, type = ?, description = ?, " +
+            " organizationAddress = ?, phone = ?, contactInfo = ? WHERE organizationPK = ?;";
+    final String DELETE_HERO_ORGANIZATION = "DELETE FROM heroorganization WHERE organizationPK = ?";
+    final String DELETE_ORGANIZATION = "DELETE FROM organization WHERE organizationPK = ?";
+    final String GET_ORGANIZATIONS_BY_HERO = "SELECT o.* FROM organization o INNER JOIN heroorganization ho ON " +
+            " o.organizationPK = ho.organizationPK WHERE ho.heroPK = ?";
+
     @Override
     public Organization getOrganizationByID(int id) {
         try{
-            final String SELECT_ORGANIZATION_BY_ID = "SELECT * " +
-                    "FROM organization " +
-                    "WHERE OrganizationPK = ?;";
-            Organization organization = jdbc.queryForObject(SELECT_ORGANIZATION_BY_ID, new OrganizationMapper(), id);
+            Organization organization = jdbc.queryForObject(GET_ORGANIZATION_BY_ID, new OrganizationMapper(), id);
             organization.setMembers(getHeroesByOrganization(organization));
             return organization;
         } catch (DataAccessException e) {
@@ -41,25 +52,18 @@ public class OrganizationDaoDB implements OrganizationDao {
     }
 
     private List<Hero> getHeroesByOrganization(Organization organization) {
-        final String SELECT_HERO_BY_ORGANIZATION = "SELECT DISTINCT h.* " +
-                "FROM hero h " +
-                "INNER JOIN heroorganization ho ON h.heroPK = ho.heroPK " +
-                "WHERE organizationPK = ?;";
-        return jdbc.query(SELECT_HERO_BY_ORGANIZATION, new HeroMapper(), organization.getId());
+        return jdbc.query(GET_HEROS_BY_ORGANIZATION, new HeroMapper(), organization.getId());
     }
 
     @Override
     public List<Organization> getAllOrganizations() {
-        final String SELECT_ALL_ORGANIZATIONS = "SELECT * FROM organization;";
-        return jdbc.query(SELECT_ALL_ORGANIZATIONS, new OrganizationMapper());
+        return jdbc.query(GET_ALL_ORGANIZATIONS, new OrganizationMapper());
     }
 
     @Override
     public Organization addOrganization(Organization organization) {
-        final String INSERT_NEW_ORGANIZATION = "INSERT INTO organization (OrganizationName, Type, Description, OrganizationAddress, Phone, ContactInfo) " +
-                "VALUES (?,?,?,?,?,?);";
         jdbc.update(
-                INSERT_NEW_ORGANIZATION,
+                INSERT_ORGANIZATION,
                 organization.getName(),
                 organization.getType(),
                 organization.getDescription(),
@@ -67,16 +71,13 @@ public class OrganizationDaoDB implements OrganizationDao {
                 organization.getPhone(),
                 organization.getContact());
 
-        int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID();", Integer.class);
+        int newId = jdbc.queryForObject(GET_INSERT_ID , Integer.class);
         organization.setId(newId);
         return organization;
     }
 
     @Override
     public void updateOrganization(Organization organization) {
-        final String UPDATE_ORGANIZATION = "UPDATE organization " +
-                "SET OrganizationName = ?, Type = ?, Description = ?, OrganizationAddress = ?, Phone = ?, ContactInfo = ? " +
-                "WHERE OrganizationPK = ?";
         jdbc.update(
                 UPDATE_ORGANIZATION,
                 organization.getName(),
@@ -92,26 +93,13 @@ public class OrganizationDaoDB implements OrganizationDao {
     @Override
     @Transactional
     public void deleteOrganizationByID(int id) {
-        // Delete from bridge table first
-        final String DELETE_HERO_ORGANIZATION = "DELETE FROM heroorganization " +
-                "WHERE OrganizationPK = ?";
-
-        // Delete from organizations table
-        final String DELETE_ORGANIZATION = "DELETE FROM organization " +
-                "WHERE OrganizationPK = ?";
-
         jdbc.update(DELETE_HERO_ORGANIZATION, id);
         jdbc.update(DELETE_ORGANIZATION, id);
     }
 
     @Override
     public List<Organization> getOrganizationsByHero(Hero hero) {
-        final String SELECT_ORGANIZATIONS_BY_HERO = "SELECT o.* " +
-                "FROM organization o " +
-                "INNER JOIN heroorganization ho " +
-                "ON o.organizationPK = ho.organizationPK " +
-                "WHERE ho.heroPK = 4";
-        List<Organization> organizations = jdbc.query(SELECT_ORGANIZATIONS_BY_HERO, new OrganizationMapper(), hero.getId());
+        List<Organization> organizations = jdbc.query(GET_ORGANIZATIONS_BY_HERO, new OrganizationMapper(), hero.getId());
         for (Organization organization : organizations) {
             organization.setMembers(getHeroesByOrganization(organization));
         }
@@ -125,8 +113,8 @@ public class OrganizationDaoDB implements OrganizationDao {
             Organization organization = new Organization();
             organization.setId(rs.getInt("OrganizationPK"));
             organization.setName(rs.getString("OrganizationName"));
-            organization.setType(rs.getString("`Type`"));
-            organization.setDescription(rs.getString("`Description`"));
+            organization.setType(rs.getString("Type"));
+            organization.setDescription(rs.getString("Description"));
             organization.setAddress(rs.getString("OrganizationAddress"));
             organization.setPhone(rs.getString("Phone"));
             organization.setContact(rs.getString("ContactInfo"));
