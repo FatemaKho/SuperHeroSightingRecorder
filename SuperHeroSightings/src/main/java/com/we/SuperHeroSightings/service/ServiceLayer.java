ec7e0e2fe14e0663526536c8f1af9e790d75a11e
strangechick <fkhondker123@gmail.com>
2023-07-19 15:20:09 -0400

    Merge branch 'main' of github.com:FatemaKho/SuperHeroSightingRecorder
    # Conflicts:
    #	SuperHeroSightings/src/main/resources/application.properties
    #	SuperHeroSightings/src/test/resources/application.properties

--- SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceLayer.java
+++ SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceLayer.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class ServiceLayer implements ServiceInterface {
+
     @Override
     public Hero getHeroByID(int id) {
         return null;
--- SuperHeroSightings/src/test/java/com/we/SuperHeroSightings/dao/SightingDaoDBTest.java
+++ SuperHeroSightings/src/test/java/com/we/SuperHeroSightings/dao/SightingDaoDBTest.java
@@ -15,11 +15,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  *
  * @author jtriolo
- */    
+ */
+
 @SpringBootTest
 public class SightingDaoDBTest {
     
