7af41dd97af5c7ada1af8cecf9cd26c876fe0a06
Lupita Peralta <cstuart@email.com>
2023-07-19 14:39:07 -0500

    sighting service layer

--- SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceInterface.java
+++ SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceInterface.java
@@ -1,12 +1,13 @@
 package com.we.SuperHeroSightings.service;
 
+import com.we.SuperHeroSightings.dao.SightingDao;
 import com.we.SuperHeroSightings.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ServiceInterface {
-
     Hero getHeroByID(int id);
     List<Hero> getAllHeros();
     Hero addHero(Hero hero);
--- SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceLayer.java
+++ SuperHeroSightings/src/main/java/com/we/SuperHeroSightings/service/ServiceLayer.java
@@ -1,11 +1,16 @@
 package com.we.SuperHeroSightings.service;
 
+import com.we.SuperHeroSightings.dao.SightingDao;
 import com.we.SuperHeroSightings.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Service
 public class ServiceLayer implements ServiceInterface {
+    @Autowired
+    SightingDao sightingDao;
     @Override
     public Hero getHeroByID(int id) {
         return null;
@@ -128,41 +133,41 @@ public class ServiceLayer implements ServiceInterface {
 
     @Override
     public Sighting getSightingByID(int id) {
-        return null;
+        return sightingDao.getSightingByID();
     }
 
     @Override
     public List<Sighting> getAllSightings() {
-        return null;
+        return sightingDao.getAllSightings();
     }
 
     @Override
     public Sighting addSighting(Sighting sighting) {
-        return null;
+        return sightingDao.addSighting();
     }
 
     @Override
     public void updateSighting(Sighting sighting) {
-
+        sightingDao.updateSighting(sighting);
     }
 
     @Override
     public void deleteSightingByID(int id) {
-
+        sightingDao.deleteSightingByID(id);
     }
 
     @Override
     public List<Sighting> getSightingsByDate(LocalDateTime date) {
-        return null;
+        return sightingDao.getSightingsByDate(date);
     }
 
     @Override
     public List<Sighting> getSightingsByLocation(Location location) {
-        return null;
+        return sightingDao.getSightingsByLocation(location);
     }
 
     @Override
     public List<Sighting> getSightingsByHero(Hero hero) {
-        return null;
+        return sightingDao.getSightingsByHero(hero);
     }
 }
